d7c0d2126ed0aeb66d0a144857d3ca3737e93e4a
Tim Katzer <timsuper@icloud.com>
2020-06-17 14:51:50 +0200

    calibration

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <avr/wdt.h>
+#include <EEPROM.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
@@ -20,17 +21,34 @@ DHT dht_2;
   and pin #52 (SS) must be an output
 */
 
-// HW-SPI wird für SD-Karte verwendet
-#define UNO_MOSI 11
-#define UNO_MISO 12
-#define UNO_CLK 13
-#define UNO_SS 10
+// SD-Karte
+#define UNO_HW_MOSI 11
+#define UNO_HW_MISO 12
+#define UNO_HW_CLK 13
+#define UNO_HW_SS 10
 
 File dataFile;
+
+// Bodenfeuchtesensoren
+
+#define BFS_1 A1
+#define BFS_2 A2
+
+#define BFS_1_BOTTOM_EEPROM_ADDR 0
+#define BFS_2_BOTTOM_EEPROM_ADDR 1
+#define BFS_1_TOP_EEPROM_ADRR 2
+#define BFS_2_TOP_EEPROM_ADRR 3
+
+// RTC
 
 unsigned long time;
 
 const int messinterval = 10000;
+
+uint8_t bfs_1_bottom_value;
+uint8_t bfs_2_bottom_value;
+uint8_t bfs_1_top_value;
+uint8_t bfs_2_top_value;
 
 /* To-Do:
 - Analog Sensoren für Bodenfeuchte
@@ -40,12 +58,14 @@ const int messinterval = 10000;
 void setup() {
   Serial.begin(9600);
 
+  // WDT
   // Register für WDT sicherheitshalber zurücksetzen
   MCUCR = 0;
 
-  pinMode(UNO_SS, OUTPUT);
+  // SD Karte
+  pinMode(UNO_HW_SS, OUTPUT);
 
-  if (!SD.begin(UNO_SS)) {
+  if (!SD.begin(UNO_HW_SS)) {
     Serial.println("SD Card failed, or not present");
     Serial.println("while(true)");
     while (1);
@@ -58,8 +78,25 @@ void setup() {
     while (1);
   }
   
+  // DHT
   dht_1.setup(DHT_1_DATA_PIN, DHT_TYPE);
   dht_2.setup(DHT_2_DATA_PIN, DHT_TYPE);
+
+  // RTC
+
+  // BFS
+  pinMode(BFS_1, INPUT);
+  pinMode(BFS_2, INPUT);
+
+  wdt_enable(WDTO_500MS);
+
+  bfs_1_bottom_value = EEPROM.read(BFS_1_BOTTOM_EEPROM_ADDR);
+  bfs_2_bottom_value = EEPROM.read(BFS_2_BOTTOM_EEPROM_ADDR);
+  bfs_1_top_value = EEPROM.read(BFS_1_TOP_EEPROM_ADRR);
+  bfs_2_top_value = EEPROM.read(BFS_2_TOP_EEPROM_ADRR);
+
+  wdt_reset();
+  wdt_disable();
 }
 
 void loop() {
@@ -88,4 +125,115 @@ void loop() {
 
   // Messintervall
   delay(messinterval);
+}
+
+bool bfs_calibrate() {
+  uint8_t bfs_1_bottom;
+  uint8_t bfs_2_bottom;
+  uint8_t bfs_1_top;
+  uint8_t bfs_2_top;
+
+  Serial.println("Kablibrierung der Bodenfeuchtesensoren");
+  Serial.println("Reihenfolge:");
+  Serial.println("1. Sensor 1 (trocken an der Luft");
+  Serial.println("2. Sensor 2 (trocken an der Luft");
+  Serial.println("3. Sensor 1 (in feuchte Erde stecken)");
+  Serial.println("4. Sensor 2 (in feuchte Erde stecken)");
+  Serial.println();
+  Serial.println("Die Werte werden im EEPROM automatisch gespeichert!");
+
+  Serial.println();
+  Serial.print("warte: ");
+
+  for (size_t i = 5; i < 0; i--)
+  {
+    Serial.print(i + "  ");
+    delay(1000);
+  }
+
+  Serial.println();
+
+  /// BFS Sensor 1 trocken ermitteln
+  Serial.println("Sensor 1 trocken:");
+  Serial.println();
+  Serial.print("warte: ");
+  
+  for (size_t i = 5; i < 0; i--)
+  {
+    Serial.print(i + "  ");
+    delay(1000);
+  }
+
+  // Messung
+  bfs_1_bottom = analogRead(BFS_1);
+  
+  Serial.println();
+  /// Ende
+
+  /// BFS Sensor 2 trocken ermitteln
+  Serial.println("Sensor 2 trocken:");
+  Serial.println();
+  Serial.print("warte: ");
+
+  for (size_t i = 5; i < 0; i--)
+  {
+    Serial.print(i + "  ");
+    delay(1000);
+  }
+
+  // Messung
+  bfs_2_bottom = analogRead(BFS_2);
+
+  Serial.println();
+  /// Ende
+
+  /// BFS Sensor 1 feucht ermitteln
+  Serial.println("Sensor 1 feucht:");
+  Serial.println();
+  Serial.print("warte: ");
+
+  for (size_t i = 5; i < 0; i--)
+  {
+    Serial.print(i + "  ");
+    delay(1000);
+  }
+
+  // Messung
+  bfs_1_top = analogRead(BFS_1);
+
+  Serial.println();
+  /// Ende
+
+  // BFS Sensor 2 feucht ermitteln
+  Serial.println("Sensor 2 feucht:");
+  Serial.println();
+  Serial.print("warte: ");
+
+  for (size_t i = 5; i < 0; i--)
+  {
+    Serial.print(i + "  ");
+    delay(1000);
+  }
+
+  // Messung
+  bfs_2_top = analogRead(BFS_2);
+
+  Serial.println();
+
+  ///Ende
+
+  Serial.println("Alle Werte wurden ermittelt!");
+  Serial.println("Werte werden in EEPROM übernommen");
+
+  wdt_enable(WDTO_500MS);
+  
+  EEPROM.update(BFS_1_BOTTOM_EEPROM_ADDR, bfs_1_bottom);
+  EEPROM.update(BFS_2_BOTTOM_EEPROM_ADDR, bfs_2_bottom);
+  EEPROM.update(BFS_1_TOP_EEPROM_ADRR, bfs_1_top);
+  EEPROM.update(BFS_2_TOP_EEPROM_ADRR, bfs_2_top);
+
+  wdt_reset();
+  wdt_disable();
+
+  return true;
 }
